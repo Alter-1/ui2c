5d9a1ba23286f2e51bd78c58b7d42a00222dbd8d
_AT_ <oleksandr.teliatnikov@vidisco.com>
2023-06-02 22:34:42 +0300

    copy files to /bin/$platform (windows)

--- c/windows/_build.cmd
+++ c/windows/_build.cmd
@@ -8,3 +8,16 @@ SET MSBUILD="%VS2019%\MSBuild\Current\Bin\MSBuild.exe"
 %MSBUILD%  build32\ui2c_project.sln -target:ui2c_static  /property:Configuration="Release"
 %MSBUILD%  build32\ui2c_project.sln -target:test_app     /property:Configuration="Release"
 
+
+SET DSTDIR=..\..\bin
+if not exist %DSTDIR% mkdir %DSTDIR%
+
+SET DSTDIR1=%DSTDIR%\Win32
+if not exist %DSTDIR1% mkdir %DSTDIR1%
+copy build32\bin\Release\* %DSTDIR1%
+copy build32\lib\Release\*.lib %DSTDIR1%
+
+SET DSTDIR1=%DSTDIR%\x64
+if not exist %DSTDIR1% mkdir %DSTDIR1%
+copy build64\bin\Release\* %DSTDIR1%
+copy build64\lib\Release\*.lib %DSTDIR1%
